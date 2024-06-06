e0df9cfd9dc3cf2b32b5499326cc54f10a59b0fc
Mahendar Dwi Payana <38557290+mahendartea@users.noreply.github.com>
2024-06-06 11:16:32 +0700

    tambah++

--- Function.md
+++ Function.md
@@ -108,5 +108,127 @@ def my_function(food):
 fruits = ["apple", "banana", "cherry"]
 ```
 
+## 8. Docstring
+
+docstring adalah sebuah teks yang menunjukkan apa yang terlihat pada sebuah `function`. Docstring ini biasanya dituliskan pada `function` yang kita buat.
+
+```python
+def suhu_udara (daerah, derajat = 30, satuan = 'celcius'):
+  """
+  Fungsi ini bertugas untuk menampilkan teks yang memberikan informasi 
+  tentang suhu udara di suatu daerah.
+  """
+
+  print("Suhu di {} adalah {} {}".format(daerah, derajat, satuan))
+```
+
+### 1. Latihan 1
+```python
+def persentase (total, jumlah):
+  if (total >= 0 and total <= jumlah):
+    return total / jumlah * 100
+  
+  return False
+
+# output 50
+print(persentase(30, 60))
+
+# output False
+print(persentase(100, 60))
+```
+
+### 2. Latihan 2
+
+```python
+# membuat variabel global
+nama = "Petani Kode"
+versi = "1.0.0"
+
+def help():
+    # ini variabel lokal
+    nama = "Programku"
+    versi = "1.0.2"
+    # mengakses variabel lokal
+    print "Nama: %s" % nama
+    print "Versi: %s" % versi
+
+
+# mengakses variabel global
+print "Nama: %s" % nama
+print "Versi: %s" % versi
+
+# memanggil fungsi help()
+help()
+
+```
+
+### 3. Latihan 3
+
+```python
+buku = []
+# fungsi untuk menampilkan semua data
+def show_data():
+    if len(buku) <= 0:
+        print("Belum ada data")
+    else:
+        for indeks in range(len(buku)):
+            print("[%d] %s" % (indeks, buku[indeks]))
+
+# fungsi untuk menambahkan data
+def tambah_buku():
+    buku_baru = input("BUKU BARU : ")
+    buku.append(buku_baru)
+    print("DATA BERHASIL DISIMPAN")
+
+# fungsi untuk edit data
+def edit_data():
+    show_data()
+    indeks = int(input("Inputkan ID buku: "))
+    if indeks >= len(buku):
+        print("ID salah")
+    else:
+        judul_baru = input("Judul baru: ")
+        buku[indeks] = judul_baru
+        print("DATA BERHASIL DIUPDATE")
+
+# fungsi untuk menghapus data
+def delete_data():
+    show_data()
+    indeks = int(input("Inputkan ID buku: "))
+    if indeks >= len(buku):
+        print("ID salah")
+    else:
+        del buku[indeks]
+
+def show_menu():
+    print("\n")
+    print("----------- MENU ----------")
+    print("[1] Show Data")
+    print("[2] Insert Data")
+    print("[3] Edit Data")
+    print("[4] Delete Data")
+    print("[5] Exit")
+    
+    menu = int(input("PILIH MENU> "))
+    print("\n")
+
+    if menu == 1:
+        show_data()
+    elif menu == 2:
+        tambah_buku()
+    elif menu == 3:
+        edit_data()
+    elif menu == 4:
+        delete_data()
+    elif menu == 5:
+        exit()
+    else:
+        print("Salah pilih!")
+
+if __name__ == "__main__":
+    while True:
+        show_menu()
+        show_menu()
+```
 
 [<<Kembali](README.md)
--- source/kondisi.py
+++ source/kondisi.py
@@ -1,98 +1,143 @@
-# kondisi if
+# # kondisi if
+# a = 10
+# if a == 11: #benar jalankan dibawahnya
+#     print("Benar")
 
-a = 10
-if a == 10:
-    print("Benar")
+# # kondisi if else
+# a = 20
+# if a == 21:
+#     print("Benar")
+# else:
+#     print("Salah")
 
-# kondisi if else
-a = 20
-if a == 20:
-    print("Benar")
-else:
-    print("Salah")
+# # kondisi if elif
+# a = 20
+# if a == 30:
+#     print("Benar 30")
+# elif a == 20:
+#     print("yang benar 20")
+# else:
+#     print("Salah")
+#
+# # kondisi dengan nilai boolean
+# a = False
+# b = True
+# c = True
 
-# kondisi if elif
-a = 30
-if a == 30:
-    print("Benar")
-elif a == 20:
-    print("Salah")
-else:
-    print("Salah")
+# if a:
+#     print("A Benar")
+# elif b:
+#     print("Salah A, tetapi B Benar")
+# elif c:
+#     print("C Benar, yang salah")
+# else:
+#     print("salah semua")
+#
+# # kondisi dengan operator logika
+# a = True
+# b = False
+# c = True
 
-# kondisi dengan nilai boolean
-a = True
-b = False
-c = True
-
-if a:
-    print("Benar")
-elif b:
-    print("Salah")
-else:
-    print("Salah")
-
-# kondisi dengan operator logika
-a = True
-b = False
-c = True
-
-if a and b:
-    print("Benar")
-elif a or b:
-    print("Salah")
-else:
-    print("Salah")
+# if a and b:
+#     print("Benar")
+# elif a or b:
+#     print("Salah 1")
+# else:
+#     print("Salah 2")
 
 # kondisi dengan operator pembanding
-a = 10
-b = 20
+# a = 10
+# b = 20
+# #
+# if a < b:
+#     print("Benar, A lebih kecil daripada B")
+# elif a > b:
+#     print("Benar A lebih besar daripada B")
+# else:
+#     print("Pernyataan Salah")
+#
+# #kondisi dengan operator identitas
+# a = 10
+# b = [1, 2, 3, 4, 5, 10]
 
-if a < b:
-    print("Benar")
-elif a > b:
-    print("Salah")
-else:
-    print("Salah")
+# if a in b:
+#     print("Ada A di dalam B")
+# elif a not in b:
+#     print("Tidak ada A di dalam B")
+#
+# #kondisi dengan operator not
+# a = False
 
-#kondisi dengan operator identitas
-a = 10
-b = [1, 2, 3, 4, 5]
+# if not a:
+#     print("Benar")
+# else:
+#     print("Salah")
+#
+# # Switch Case
+# print('''
+# 1. Pemrograman Framework
+# 2. Datamining
+# 3. Network Security
+# 4. Computer Vision
+# 5. SIG
+# lainnya. Gak kuliah lagi
 
-if a in b:
-    print("Benar")
-elif a not in b:
-    print("Salah")
+# ''')
+# inputan = input("Masukkan angka: ")
 
-#kondisi dengan operator not
-a = 10
+# match inputan:
+#     case "1":
+#         print("Pemrograman Framework \t : 3 SKS")
+#     case "2":
+#         print("Datamining \t : 3 SKS")
+#     case "3":
+#         print("Network Security \t : 3 SKS")
+#     case "4":
+#         print("Computer Vision \t : 3 SKS")
+#     case "5":
+#         print("SIG \t : 3 SKS")
+#     case _:
+#         print("Gak kuliah lagi \t : DO")
+#
+# # Ternary Operator
+# a = 10
 
-if not a:
-    print("Benar")
-else:
-    print("Salah")
+# print("Benar") if a == 10 else print("Salah")
+#
+#
+#
+# nim = int(input("Masukan Nomor Induk Mahasiswa : "))
+# nama = str(input("Masukan Nama Mahasiswa: "))
+# nilai = int(input("Masukan nilai final: "))
+# if nilai >= 80:
+#     status = "Lulus"
+# else:
+#     status = "Tidak Lulus"
 
-# Switch Case
-inputan = input("Masukkan angka: ")
+# print("=============================")
+# print("Nomor induk mahasiswa : ", nim)
+# print("Nama lengkap mahasiswa : ", nama)
+# print("Nilai \t: ", nilai)
+# print("Status \t: ", status)
 
-match inputan:
-    case "1":
-        print("Satu")
-    case "2":
-        print("Dua")
-    case "3":
-        print("Tiga")
-    case "4":
-        print("Empat")
-    case "5":
-        print("Lima")
-    case _:
-        print("Salah")
+# nim = int(input("Masukan Nomor Induk Mahasiswa : "))
+# nama = str(input("Masukan Nama Mahasiswa: "))
+# tugas = int(input("Masukan nilai Tugas: "))
+# uts = int(input("Masukan nilai UTS: "))
+# uas = int(input("Masukan nilai UAS: "))
+# hasil = (0.2*tugas)+(0.3*uts)+(0.5*uas)
 
-# Ternary Operator
-a = 10
+# if hasil >= 80:
+#     status = "Lulus"
+# else:
+#     status = "Tidak Lulus"
 
-print("Benar") if a == 10 else print("Salah")
-
-
+# print("=============================")
+# print("Nomor induk mahasiswa : ", nim)
+# print("Nama lengkap mahasiswa : ", nama)
+# print("Nilai Tugas \t: ", tugas)
+# print("Nilai UTS \t: ", uts)
+# print("nilai UAS \t: ", uas)
+# print("Nilai Akhir adalah : ", hasil)
+# print("Keterangan : ", status)
 
